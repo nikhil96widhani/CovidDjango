405e4ccaefa11a02e87aaaefd8af7bcb31959973
Nikhil Widhani <46464842+nikhil96widhani@users.noreply.github.com>
2020-05-19 16:17:48 -0400

    added favicon

--- covidapp/services.py
+++ covidapp/services.py
@@ -35,54 +35,54 @@ def get_country_names():
 
     return data
 
-#
-# class GetNews:
-#     def __init__(self, country, no_of_articles):
-#         self.country = country.lower()
-#         self.no_of_articles = no_of_articles
-#         self.get_country_code()
-#
-#     def get_country_code(self):
-#         url = "https://covid-19-data.p.rapidapi.com/help/countries"
-#
-#         querystring = {"format": "json"}
-#
-#         headers = {
-#             'x-rapidapi-host': "covid-19-data.p.rapidapi.com",
-#             'x-rapidapi-key': "e0d880658amsh5707d8639e35fc3p1e7243jsn0bc77771acc8"
-#         }
-#
-#         response = requests.request("GET", url, headers=headers, params=querystring)
-#
-#         for x in response.json():
-#             try:
-#                 if x['name'].lower() == self.country:
-#                     self.country_code = x['alpha2code'].lower()
-#             except:
-#                 self.country_code = ''
-#
-#     def get_articles(self):
-#         try:
-#             top_headlines = newsapi.get_top_headlines(q='Covid-19',
-#                                                       # language='en',
-#                                                       country=self.country_code
-#                                                       )
-#             news_headlines = top_headlines['articles'][:self.no_of_articles]
-#
-#             if news_headlines:
-#                 return news_headlines
-#             else:
-#                 top_headlines = newsapi.get_everything(q='Covid-19 {}'.format(self.country),
-#                                                        # language='en',
-#                                                        # country=country_code
-#                                                        )
-#                 return top_headlines['articles'][:self.no_of_articles]
-#         except:
-#             top_headlines = newsapi.get_everything(q='Covid-19 {}'.format(self.country),
-#                                                    # language='en',
-#                                                    # country=country_code
-#                                                    )
-#             return top_headlines['articles'][:self.no_of_articles]
+
+class GetNews:
+    def __init__(self, country, no_of_articles):
+        self.country = country.lower()
+        self.no_of_articles = no_of_articles
+        self.get_country_code()
+
+    def get_country_code(self):
+        url = "https://covid-19-data.p.rapidapi.com/help/countries"
+
+        querystring = {"format": "json"}
+
+        headers = {
+            'x-rapidapi-host': "covid-19-data.p.rapidapi.com",
+            'x-rapidapi-key': "e0d880658amsh5707d8639e35fc3p1e7243jsn0bc77771acc8"
+        }
+
+        response = requests.request("GET", url, headers=headers, params=querystring)
+
+        for x in response.json():
+            try:
+                if x['name'].lower() == self.country:
+                    self.country_code = x['alpha2code'].lower()
+            except:
+                self.country_code = ''
+
+    def get_articles(self):
+        try:
+            top_headlines = newsapi.get_top_headlines(q='Covid-19',
+                                                      # language='en',
+                                                      country=self.country_code
+                                                      )
+            news_headlines = top_headlines['articles'][:self.no_of_articles]
+
+            if news_headlines:
+                return news_headlines
+            else:
+                top_headlines = newsapi.get_everything(q='Covid-19 {}'.format(self.country),
+                                                       # language='en',
+                                                       # country=country_code
+                                                       )
+                return top_headlines['articles'][:self.no_of_articles]
+        except:
+            top_headlines = newsapi.get_everything(q='Covid-19 {}'.format(self.country),
+                                                   # language='en',
+                                                   # country=country_code
+                                                   )
+            return top_headlines['articles'][:self.no_of_articles]
 
 
 
